77339276a0be1ec7d8d664324566896bb99531ad
ch3rryl <cherryl@live.co.uk>
2024-01-11 15:47:32 +0000

    complete junit tests for calculator

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -4,23 +4,40 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@DisplayName(value = "The Calculator should")
+@DisplayName("What the calculator should do")
 public class CalculatorTests {
 
     @Test
-    @DisplayName("add two numbers")
-    public void testAddition() {
-        final Integer sum = Calculator.add(2, 4);
-
-        assertEquals(6, sum);
+    @DisplayName("add two nums")
+    void addSum() {
+        int addnumSum = Calculator.add(2, 4);
+        assertEquals(6, addnumSum);
     }
 
     @Test
-    @DisplayName("subtract two numbers")
-    public void testSubtraction() {
-        final Integer difference = Calculator.subtract(2, 4);
-
-        assertEquals(-2, difference);
+    @DisplayName("divide nums")
+    void sumDivide() {
+        int divideSum = Calculator.divide(6,2);
+        assertEquals(3, divideSum);
     }
+
+    //test to catch errors where dividing by zero
+    @Test
+    @DisplayName("arithmetic exception division by zero")
+    void setArithmeticException() {
+    Exception arythmaticException = assertThrows(ArithmeticException.class, () -> Calculator.divide(1,0));
+
+    assertEquals("/ by zero", arythmaticException.getMessage());
 }
+
+@Test
+    @DisplayName("sum of multiplication")
+
+    void multiplySum () {
+        int multiplySum = Calculator.multiply(2,2);
+        assertEquals(4, multiplySum);
+}
+}
+
